3d3400880c25a72249d2f5f25417c63ddf724c0e
tvthinit <tvt.thin.it@gmail.com>
2026-09-10 08:37:13 +0700

    update shows fields, search box for that all pages

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
+from django.db.models import CharField, F, Q
+from django.db.models.functions import Cast
+
 from .models import Booking, GuestProfile, Hotel, HotelImage, Payment, Promotion, Refund, Review, Room
 
-admin.site.register(Hotel)
-admin.site.register(HotelImage)
-admin.site.register(Booking)
-admin.site.register(Room)
-admin.site.register(GuestProfile)
-admin.site.register(Promotion)
-admin.site.register(Payment)
-admin.site.register(Refund)
-admin.site.register(Review)
+
+def admin_with_all_fields(model):
+	class AllFieldsAdmin(admin.ModelAdmin):
+		list_display = tuple(field.name for field in model._meta.fields)
+		search_fields = ('id',)
+		list_per_page = 50
+
+		def get_search_results(self, request, queryset, search_term):
+			if not search_term:
+				return queryset, False
+
+			annotations = {
+				f'_admin_search_{field.name}': Cast(F(field.name), output_field=CharField())
+				for field in model._meta.fields
+			}
+			queryset = queryset.annotate(**annotations)
+			filters = Q()
+			for field in model._meta.fields:
+				filters |= Q(**{f'_admin_search_{field.name}__icontains': search_term})
+			return queryset.filter(filters), False
+
+	return AllFieldsAdmin
+
+
+for model in (Hotel, HotelImage, Booking, Room, GuestProfile, Promotion, Payment, Refund, Review):
+	admin.site.register(model, admin_with_all_fields(model))
